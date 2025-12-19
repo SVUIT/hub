d115bc446c85b95b1aa7f317a385040ae6e84b02
Calvinan132 <trinhhoaian2303@gmail.com>
2025-12-19 20:56:28 +0700

    chore: upgrade docusaurus to latest version

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,64 +1,68 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'UIT Resource Hub',
-  tagline: 'Website tổng hợp và cập nhật các ưu đãi phần mềm bản quyền cho sinh viên UIT.',
-  favicon: 'img/favicon.ico',
+  title: "UIT Resource Hub",
+  tagline:
+    "Website tổng hợp và cập nhật các ưu đãi phần mềm bản quyền cho sinh viên UIT.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/hub/',
+  baseUrl: "/hub/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
+  onBrokenLinks: "throw",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           //editUrl:
-            //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -66,38 +70,41 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'SVUIT',
+      title: "SVUIT",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.png',
+        alt: "My Site Logo",
+        src: "img/logo.png",
       },
       items: [
         //{
-         // type: 'docSidebar',
-          //sidebarId: 'tutorialSidebar',
-          //position: 'left',
-          //label: 'Tutorial',
+        // type: 'docSidebar',
+        //sidebarId: 'tutorialSidebar',
+        //position: 'left',
+        //label: 'Tutorial',
         //},
-          { href: 'https://svuit.org/mmtt/', 
-            label: 'Kho tài liệu', 
-            position: 'right', 
-          },
-          { href: 'https://svuit.org/mmtt/docs/contribute', 
-            label: 'Đóng góp', 
-            position: 'right', 
-          },
-          { href: 'https://svuit.org/mmtt/docs/ThongBao/index', 
-            label: 'Thông báo', 
-            position: 'right', 
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [], // Xóa để tránh Docusaurus render cột mặc định
-        copyright: `
+        {
+          href: "https://svuit.org/mmtt/",
+          label: "Kho tài liệu",
+          position: "right",
+        },
+        {
+          href: "https://svuit.org/mmtt/docs/contribute",
+          label: "Đóng góp",
+          position: "right",
+        },
+        {
+          href: "https://svuit.org/mmtt/docs/ThongBao/index",
+          label: "Thông báo",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [], // Xóa để tránh Docusaurus render cột mặc định
+      copyright: `
         <div class="custom-footer">
           <div class="footer-title">STUDY VAULT OF UIT</div>
           <div class="footer-links">
@@ -120,8 +127,7 @@ const config: Config = {
           </div>
         </div>
       `,
-
-      },
+    },
 
     prism: {
       theme: prismThemes.github,
